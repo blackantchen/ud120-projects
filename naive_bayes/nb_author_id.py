49deb8630abbccd8940fd403d853ed6c33e75129
blackantchen <whatwhereme@sohu.com>
2017-11-15 14:04:35 +0800

    add naive-bayes exercise code, using scikit-learn lib

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python
 
-""" 
-    This is the code to accompany the Lesson 1 (Naive Bayes) mini-project. 
+"""
+    This is the code to accompany the Lesson 1 (Naive Bayes) mini-project.
 
     Use a Naive Bayes Classifier to identify emails by their authors
-    
+
     authors and labels:
     Sara has label 0
     Chris has label 1
 """
-    
+
 import sys
 from time import time
 sys.path.append("../tools/")
@@ -26,8 +26,21 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+from sklearn.naive_bayes import MultinomialNB
+from sklearn import metrics
 
+t0 = time()
+# Build Classifier and load datasets into
+clf = MultinomialNB().fit(features_train, labels_train)
+print "training time:", round(time()-t0,3),"s"
+
+t0 = time()
+# use new Classifier to class test datasets
+test_classed = clf.predict(features_test)
+print "predict time:", round(time()-t0,3),"s"
+
+# show the precision
+expect_y = labels_test
+print(metrics.classification_report(expect_y, test_classed, target_names={'chris','sara'}))
 
 #########################################################
-
-
