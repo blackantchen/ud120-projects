1e060e9461e3c33e6c9e41cd6a35891d1b4ef251
blackantchen <whatwhereme@sohu.com>
2017-11-16 17:03:24 +0800

    SVM: 1.add linear + mini training set 2.add rbf-svm exercise and adjust C parameter

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+#-*-coding:utf-8-*-
 
 """
     This is the code to accompany the Lesson 2 (SVM) mini-project.
@@ -25,19 +26,95 @@ features_train, features_test, labels_train, labels_test = preprocess()
 #########################################################
 ### your code goes here ###
 from sklearn import svm
+from sklearn import metrics
+import numpy as np
 
-# Create Classifier model
-clf = svm.SVC(kernel="linear")
+# 将训练数据集切割到原来的1%, 丢掉99%的数据，再次测试training time and precision
+def svm_clf_with_wholeTrainDataSet():
+    # Create Classifier model
+    clf = svm.SVC(kernel="linear")
 
-# Fit: load training datasets
-t0 = time()
-clf.fit(features_train, labels_train)
-print "training time:", round(time()-t0, 6),"s"
+    # Fit: load training datasets
+    t0 = time()
+    clf.fit(features_train, labels_train)
+    print "training time:", round(time()-t0, 6),"s"
 
-# Predict:
-t0 = time()
-pred_result = clf.predict(features_test)
-print "training time:", round(time()-t0,3),"s"
+    # Predict:
+    t0 = time()
+    pred_result = clf.predict(features_test)
+    print "predict time:", round(time()-t0,3),"s"
 
-print(metrics.classification_report(labels_test, pred_result, target_names={'chris','sara'}))
+    #show the precision of new Classifier
+    print(metrics.accuracy_score(labels_test,pred_result))
+
+def svm_clf_with_miniTrainDataSet():
+    features_train_min = features_train[:len(features_train)/100]
+    labels_train_min = labels_train[:len(labels_train)/100]
+    print "features_train_min len:",len(features_train_min),"labels_train_min len:",len(labels_train_min)
+
+    # Create Classifier model
+    clf = svm.SVC(kernel="linear")
+
+    # Fit: load training datasets
+    t0 = time()
+    clf.fit(features_train_min, labels_train_min)
+    print "training time:", round(time()-t0, 6),"s"
+
+    # Predict:
+    t0 = time()
+    pred_result = clf.predict(features_test)
+    print "predict time:", round(time()-t0,3),"s"
+
+    #show the precision of new Classifier
+    print "svm-linear accuracy:"
+    print(metrics.accuracy_score(labels_test,pred_result))
+
+def svm_clf_rbf():
+    features_train_min = features_train
+    labels_train_min = labels_train
+    # features_train_min = features_train[:len(features_train)/100]
+    # labels_train_min = labels_train[:len(labels_train)/100]
+    #print "features_train_min len:",len(features_train_min),"labels_train_min len:",len(labels_train_min)
+    print "features_train len:",len(features_train_min),"labels_train len:",len(labels_train_min)
+
+    # Create Classifier model
+    para_c = 10000.
+    clf = svm.SVC(kernel="rbf", C=para_c)
+    print "SVC para c = ", para_c
+
+    # Fit: load training datasets
+    t0 = time()
+    clf.fit(features_train_min, labels_train_min)
+    print "training time:", round(time()-t0, 6),"s"
+
+    # Predict:
+    t0 = time()
+    pred_result = clf.predict(features_test)
+    print "predict time:", round(time()-t0,3),"s"
+
+    #show the precision of new Classifier
+    print "svm-rbf accuracy:"
+    print(metrics.accuracy_score(labels_test,pred_result))
+
+    print "pred[10] = ", pred_result[10]
+    print "pred[26] = ", pred_result[26]
+    print "pred[50] = ", pred_result[50]
+
+    # print "now, predict the single test point:"
+    #
+    # pred = clf.predict(features_test[10])
+    # print "predict features_test[10]:", pred
+    #
+    # pred = clf.predict(features_test[26])
+    # print "predict features_test[26]:", pred
+    #
+    # pred = clf.predict(features_test[10])
+    # print "predict features_test[50]:", pred
+
+    print(metrics.classification_report(labels_test, pred_result, target_names={'chris','sara'}))
+
+    email_num_chris = np.sum(pred_result==1)
+    print "emails belong Chris is:", email_num_chris
+
+    return clf
 #########################################################
