4b63fe2d28379e93b0cb0bd1ea44415695381328
blackantchen <whatwhereme@sohu.com>
2017-11-23 10:48:18 +0800

    修改特征数加速分类

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -27,35 +27,22 @@ features_train, features_test, labels_train, labels_test = preprocess()
 from sklearn import tree
 from sklearn import metrics
 
-# def DT_Classify(your_splite, features_train, labels_train):
-#     clf = tree.DecisionTreeClassifier(min_samples_split=your_splite)
-#     clf = clf.fit(features_train, labels_train)
-#
-#     return clf
+def check_dt_clf_accuracy(your_splite):
+    clf = tree.DecisionTreeClassifier(min_samples_split=your_splite)
+    print"min_samples_split = ", your_splite
 
-# def check_dt_clf_accuracy(min_sample_split):
-#     clf_min_samples_split = DT_Classify(min_sample_split, features_train, labels_train)
-#     pred = clf_min_samples_split.predict(feature_test)
-#
-#     acc_min_sample_split = metrics.accuracy_score(labels_test, pred)
-#     print("accuracy is ", acc_min_sample_split)
+    t0 = time()
+    clf = clf.fit(features_train, labels_train)
+    print"training time:", round(time()-t0, 6),"s"
 
+    t0 = time()
+    pred = clf.predict(features_test)
+    print"predict time:", round(time()-t0, 6),"s"
 
-# clf_min_samples_split_2 = DT_Classify(2,features_train, labels_train)
-#
-# pred_2 = clf_min_samples_split_2.predict(features_test)
-#
-# acc_min_samples_split_2 = metrics.accuracy_score(labels_test, pred_2)
+    acc_min_sample_split = metrics.accuracy_score(labels_test, pred)
+    print"accuracy is ", acc_min_sample_split
 
-## min_samples_split = 50
-#clf_min_samples_split_50 = DT_Classify(50, features_train, labels_train)
-clf_min_samples_split_50 = tree.DecisionTreeClassifier(min_samples_split=50)
-clf_min_samples_split_50 = clf_min_samples_split_50.fit(features_train, labels_train)
+print'number of features:', len(features_train[0])
 
-pred_50 = clf_min_samples_split_50.predict(features_test)
-
-acc_min_samples_split_50 = metrics.accuracy_score(labels_test, pred_50)
-
-print "acc_min_sample_split_50:", acc_min_samples_split_50
-
+check_dt_clf_accuracy(40)
 #########################################################
